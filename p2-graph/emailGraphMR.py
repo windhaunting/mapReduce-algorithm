f6da438322252db1c38cce3b6f2ab43a7f235386
fubao <freadream@gmail.com>
2018-03-26 21:59:08 -0400

    update_mrNode

--- p2-graph/emailGraphMR.py
+++ p2-graph/emailGraphMR.py
@@ -15,21 +15,27 @@ from mrjob.step import MRStep
 
 #  • Number of nodes in the graph 
 
-class MRGraph(MRJob):
+class MRNumberNode(MRJob):
 
     def steps(self):
         return [
-            MRStep(mapper=self.mapper_get_largest,
-                   reducer=self.reducer_get_largest),     
+            MRStep(mapper=self.mapper_get_numberNode,
+                   reducer=self.reducer_get_numberNode),   
+            MRStep(reducer=self.reducer_get_final_numberNode)
             ]
 
     def mapper_get_largest(self, _, line):
         
         for inte in line.split():
-            yield 1, int(inte.strip())
+            yield inte.strip('\t')[0], 1
+            yield inte.strip('\t')[1], 1
+
         #yield "integer", len(line.split())
     
-    def reducer_get_largest(self, key, values):
-        yield "the largest integer: ", (max(values))              # max(values) indicates the largest integer
+    def reducer_get_numberNode(self, key, values):
+        yield values, key
+    
+    def reducer_get_final_numberNode(self, key, values):
+        yield "number of nodes: ", (sum(values))              # max(values) indicates the largest integer
         #yield key, values
 
