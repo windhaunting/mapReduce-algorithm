d8d85307b35a335f30cda454c00d15e078850453
fubao <freadream@gmail.com>
2018-03-27 16:18:49 -0400

    update_mrNode

--- p2-graph/emailGraphMR.py
+++ p2-graph/emailGraphMR.py
@@ -47,9 +47,62 @@ class MRNumberNode(MRJob):
         cnt = 0
         for i in values:
             cnt += 1
-        yield "number of nodes: ", cnt              # max(values) indicates the largest integer
+        yield "number of nodes: ", cnt              # len(values) indicates the largest integer
+  
+        
 
+#• Average (and median) indegree and out degree [10]
 
+class MRAverageMedianDegree(MRJob):
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_InputInDegree,
+                   reducer=self.reducer_get_eachInDegree),  
+            MRStep(mapper=self.mapper_get_averageInDegree,
+                   reducer=self.reducer_get_averageInDegree), 
+            ]
+
+    def mapper_get_InputInDegree(self, _, line):
+        lines = line.strip().split('\t')
+        #print ('linessssssss: ', line)  
+        #for ids in lines:
+        try:
+            srcId = int(lines[0])
+            dstId = int(lines[1])
+        except:
+            #print("skipping line with value", lines)
+            pass
+        else:
+            yield dstId, srcId              #indegree
+            #yield dstId, srcId
+           # yield dstId, 1
+
+        #yield "integer", len(line.split())
+    
+    def reducer_get_eachInDegree(self, key, values):
+        cnt = 0
+        for i in values:
+            cnt += 1
+        yield key, cnt
+    
+    def mapper_get_averageInDegree(self, key, cnt):
+        yield 1, cnt
+    
+    def reducer_get_averageInDegree(self, key, values):
+        # get mean of indegree
+        cnt = v = 0
+        for i in values:
+            cnt += 1          # sum of count
+            v += i            # sum of integer
+        yield "average of indegree :", float(v)/float(cnt)      # 
+        
+        # get median of indegree
+        sortedValues = sorted(values)
+        yield "median of indegree :", sortedValues 
+    
 if __name__ == '__main__':
     # 2. Number of nodes in the graph 
-     MRNumberNode.run()
+     #MRNumberNode.run()
+     
+     MRAverageMedianDegree.run()
