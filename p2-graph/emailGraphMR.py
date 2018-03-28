06d7bb532dc2e0da890160b8e654585138d907f8
fubao <freadream@gmail.com>
2018-03-28 00:14:52 -0400

    update_mrEmail

--- p2-graph/emailGraphMR.py
+++ p2-graph/emailGraphMR.py
@@ -75,14 +75,15 @@ class MRAverageMedianInDegree(MRJob):
             pass
         else:
             yield dstId, srcId              #indegree
-            #yield srcId, dstId, srcId
+            yield srcId, -1                # indegree 0
 
         #yield "integer", len(line.split())
     
     def reducer_get_eachInDegree(self, key, values):
         cnt = 0
-        for i in values:
-            cnt += 1
+        for v in values:
+            if v != -1:
+                cnt += 1
         yield key, cnt
     
     def mapper_get_averageInDegree(self, key, cnt):
@@ -91,18 +92,19 @@ class MRAverageMedianInDegree(MRJob):
     def reducer_get_averageInDegree(self, key, values):
         # get mean of indegree
         lst = list(values)
+        yield "average of indegree :", float(sum(lst))/float(len(lst))      # 
+        
+        
         # get median of indegree
         medianIndegree = 0
         sortedValues = sorted(lst)
         index = (len(sortedValues) -1) // 2
-        print ("sortedValues: ", sortedValues, index)
+        #print ("sortedValues: ", sortedValues, index)
         if len(sortedValues) % 2 == 0:
             medianIndegree =  (sortedValues[index] + sortedValues[index+1])/2.0
         else:
             medianIndegree = sortedValues[index]
             
-            
-        yield "average of indegree :", float(sum(lst))/float(len(lst))      # 
         yield "median of indegree :", medianIndegree 
 
 #out degree
@@ -127,15 +129,15 @@ class MRAverageMedianOutDegree(MRJob):
             #print("skipping line with value", lines)
             pass
         else:
-            #yield dstId, srcId              #indegree
             yield srcId, dstId              #outdegree
+            yield dstId, -1                 # outdegree 0
 
-        #yield "integer", len(line.split())
     
     def reducer_get_eachOutdegree(self, key, values):
         cnt = 0
-        for i in values:
-            cnt += 1
+        for v in values:
+            if v != -1:
+                cnt += 1
         yield key, cnt
     
     def mapper_get_averageOutdegree(self, key, cnt):
@@ -143,29 +145,80 @@ class MRAverageMedianOutDegree(MRJob):
     
     def reducer_get_averageOutdegree(self, key, values):
         # get mean of indegree
-        #print ("list:ddd ", list(values))
-        #lstOutDegree = list(values)[::]
-        # get median of indegree
         lst = list(values)
-        # get median of indegree
         sortedValues = sorted(lst)
+        yield "average of outdegree :", float(sum(lst))/float(len(lst))      # 
         
+        
+        # get median of indegree
         medianOutdegree = 0
-        print ("lstOutDegree: ", sortedValues)
         index = (len(sortedValues) -1) // 2
-        print ("sortedValues: ", sortedValues, index)
+        #print ("sortedValues: ", sortedValues, index)
+        
         if len(sortedValues) % 2 == 0:
             medianOutdegree =  (sortedValues[index] + sortedValues[index+1])/2.0
         else:
             medianOutdegree = sortedValues[index]
             
-        yield "average of outdegree :", float(sum(sortedValues))/float(len(sortedValues))      # 
         yield "median of outdegree :", medianOutdegree 
 
 
+# • Average (and median) number of nodes reachable in two hops [15]
+
+class MRTwohops(MRJob):
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_twoHopsFirst,
+                   reducer=self.reducer_get_twoHopsFirst),  
+            #MRStep(mapper=self.mapper_get_twoHopsSecond,
+            #       reducer=self.reducer_get_twoHopsSecond), 
+            ]
+
+    def mapper_get_twoHopsFirst(self, _, line):
+        lines = line.strip().split('\t')
+        #print ('linessssssss: ', line)  
+        #for ids in lines:
+        try:
+            srcId = int(lines[0])
+            dstId = int(lines[1])
+        except:
+            #print("skipping line with value", lines)
+            pass
+        else:
+            yield srcId, ([], [dstId])                 #values:  (indegree, outDegree)
+            yield dstId, ([srcId], [])                 
+    
+    def reducer_get_twoHopsFirst(self, key, values):
+    
+        #yield key, list(values)
+        
+        inDegrees = []
+        outDegrees = []
+        inDegrees += list(values)[0]
+        outDegrees = list(values)[1]
+        yield key, (inDegrees, outDegrees)
+        
+    def mapper_get_twoHopsSecond(self, key, cnt):
+        yield key, cnt
+    
+    def reducer_get_twoHopsSecond(self, key, values):
+        # get avearge of nodes in two hops
+       
+        yield key, values 
+
+
+
+# Average (and median) number of nodes reachable in two hops
+
 if __name__ == '__main__':
-    # 2. Number of nodes in the graph 
+     # 2. Number of nodes in the graph 
      #MRNumberNode.run()
      
+     # Average (and median) indegree and out degree [10]
      #MRAverageMedianInDegree.run()
-     MRAverageMedianOutDegree.run()
+     #MRAverageMedianOutDegree.run()
+     
+     #• Average (and median) number of nodes reachable in two hops [15]
+     MRTwohops.run()
+     
